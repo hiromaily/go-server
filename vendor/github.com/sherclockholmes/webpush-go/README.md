0a0a4c94749cd1ef7d2ff041143bc38c2abf17fc
hiromaily <hiromaily2@gmail.com>
2017-08-28 21:07:49 +0200

    added for travis test

---
+++ vendor/github.com/sherclockholmes/webpush-go/README.md
@@ -0,0 +1,78 @@
+# webpush-go
+
+[![Go Report Card](https://goreportcard.com/badge/github.com/SherClockHolmes/webpush-go)](https://goreportcard.com/report/github.com/SherClockHolmes/webpush-go)
+[![GoDoc](https://godoc.org/github.com/SherClockHolmes/webpush-go?status.svg)](https://godoc.org/github.com/SherClockHolmes/webpush-go)
+
+Web Push API Encryption with VAPID support.
+
+```bash
+go get -u github.com/SherClockHolmes/webpush-go
+```
+
+## Example
+
+```golang
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+
+	webpush "github.com/sherclockholmes/webpush-go"
+)
+
+const (
+	vapidPrivateKey = "<YOUR VAPID PRIVATE KEY>"
+)
+
+func main() {
+	subJSON := `{<YOUR SUBSCRIPTION JSON>}`
+
+	// Decode subscription
+	s := webpush.Subscription{}
+	if err := json.NewDecoder(bytes.NewBufferString(subJSON)).Decode(&s); err != nil {
+		log.Fatal(err)
+	}
+
+	// Send Notification
+	_, err := webpush.SendNotification([]byte("Test"), &s, &webpush.Options{
+		Subscriber:      "<EMAIL@EXAMPLE.COM>",
+		VAPIDPrivateKey: vapidPrivateKey,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+```
+
+### Generating VAPID Keys
+
+Use the helper method `GenerateVAPIDKeys` to generate the VAPID key pair.
+
+```golang
+privateKey, publicKey, err := webpush.GenerateVAPIDKeys()
+if err != nil {
+    // TODO: Handle failure!
+}
+```
+
+## Development
+
+1. Install [Go 1.8+](https://golang.org/) ([gvm](https://github.com/moovweb/gvm) recommended)
+2. `go get -u github.com/golang/dep/cmd/dep`
+3. `dep ensure`
+
+## References
+
+For more information visit these [Google Developers](https://developers.google.com/web) links:
+
+[https://developers.google.com/web/updates/2016/03/web-push-encryption](https://developers.google.com/web/updates/2016/03/web-push-encryption)  
+[https://developers.google.com/web/updates/2016/07/web-push-interop-wins](https://developers.google.com/web/updates/2016/03/web-push-encryption)
+
+### Similar Projects / Inspired By
+
+- [Push Encryption (Go)](https://github.com/GoogleChrome/push-encryption-go)
+- [go-ecdh](https://github.com/wsddn/go-ecdh)
+- [WebPush Libs](https://github.com/web-push-libs)
+- [Web Push: Data Encryption Test Page](https://jrconlin.github.io/WebPushDataTestPage/)
---
+++ vendor/github.com/sherclockholmes/webpush-go/vapid.go
@@ -0,0 +1,105 @@
+package webpush
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"math/big"
+	"net/http"
+	"net/url"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+// GenerateVAPIDKeys will create a private and public VAPID key pair
+func GenerateVAPIDKeys() (privateKey, publicKey string, err error) {
+	// Get the private key from the P256 curve
+	curve := elliptic.P256()
+
+	private, x, y, err := elliptic.GenerateKey(curve, rand.Reader)
+	if err != nil {
+		return
+	}
+
+	public := elliptic.Marshal(curve, x, y)
+
+	// Convert to base64
+	publicKey = base64.RawURLEncoding.EncodeToString(public)
+	privateKey = base64.RawURLEncoding.EncodeToString(private)
+
+	return
+}
+
+// Generates the ECDSA public and private keys for the JWT encryption
+func generateVAPIDHeaderKeys(privateKey []byte) (*ecdsa.PublicKey, *ecdsa.PrivateKey) {
+	// Public key
+	curve := elliptic.P256()
+	px, py := curve.ScalarMult(
+		curve.Params().Gx,
+		curve.Params().Gy,
+		privateKey,
+	)
+
+	pubKey := ecdsa.PublicKey{
+		Curve: curve,
+		X:     px,
+		Y:     py,
+	}
+
+	// Private key
+	d := &big.Int{}
+	d.SetBytes(privateKey)
+
+	return &pubKey, &ecdsa.PrivateKey{
+		PublicKey: pubKey,
+		D:         d,
+	}
+}
+
+// Sign the http.Request with the required VAPID headers
+func vapid(req *http.Request, s *Subscription, options *Options) error {
+	// Create the JWT token
+	subURL, err := url.Parse(s.Endpoint)
+	if err != nil {
+		return err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.MapClaims{
+		"aud": fmt.Sprintf("%s://%s", subURL.Scheme, subURL.Host),
+		"exp": time.Now().Add(time.Hour * 12).Unix(),
+		"sub": fmt.Sprintf("mailto:%s", options.Subscriber),
+	})
+
+	// ECDSA
+	b64 := base64.RawURLEncoding
+	decodedVapidPrivateKey, err := b64.DecodeString(options.VAPIDPrivateKey)
+	if err != nil {
+		return err
+	}
+
+	pubKey, privKey := generateVAPIDHeaderKeys(decodedVapidPrivateKey)
+
+	// Sign token with key
+	tokenString, err := token.SignedString(privKey)
+	if err != nil {
+		return err
+	}
+
+	// Set VAPID headers
+	req.Header.Set("Authorization", fmt.Sprintf("WebPush %s", tokenString))
+
+	vapidPublicKeyHeader := elliptic.Marshal(pubKey.Curve, pubKey.X, pubKey.Y)
+	req.Header.Set(
+		"Crypto-key",
+		fmt.Sprintf(
+			"%s;p256ecdsa=%s",
+			req.Header.Get("Crypto-Key"),
+			base64.RawURLEncoding.EncodeToString(vapidPublicKeyHeader),
+		),
+	)
+
+	return nil
+}
